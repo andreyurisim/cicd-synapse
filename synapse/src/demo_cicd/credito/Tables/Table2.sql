bb990459a733ad28345812cf45f8627f4532d0c4
André Yuri <yuri_sim@live.com>
2021-09-01 08:58:43 -0300

    alterada tabela1 e adicionada tabela2

--- synapse/src/demo_cicd/credito/Tables/Table2.sql
+++ synapse/src/demo_cicd/credito/Tables/Table2.sql
@@ -1,8 +1,7 @@
 CREATE TABLE [credito].[Table1] (
     [Id]          INT           NOT NULL,
     [ColumnName1] NVARCHAR (50) NOT NULL,
-    [ColumnName2] NVARCHAR (50) NOT NULL,
-    [ColumnName3] NVARCHAR (50) NOT NULL
+    [ColumnName2] NVARCHAR (50) NOT NULL
 );
 
 
